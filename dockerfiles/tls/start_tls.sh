abed84aa72b8c601d5f85ba2bfce228cbeffc1e8
Vitalij Ruge <vruge@phoenixcontact-sb.io>
2022-09-07 11:46:41 +0200

    fix: tls start

--- dockerfiles/tls/start_tls.sh
+++ dockerfiles/tls/start_tls.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp ../../certs/server/hivemq.jks ./hivemq.jks
+rm -f hivemq.jks || echo "no clean"
+keytool -genkey -keyalg RSA -alias hivemq -keystore hivemq.jks -storepass betterChangeMe -validity 360 -keysize 2048 -dname "CN=localhost, OU=Foo, O=Bar, L=City, ST=State, C=DE"
 podman build -t hivemq_tls -f Dockerfile --platform=linux/amd64
 podman kill $(podman ps -q) || echo "no kill"
 podman run -p 8080:8080 -p 1883:1883 -p 8883:8883 -p 8081:8081 localhost/hivemq_tls
